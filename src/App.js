628181c3348e29e2a29020df4959aa20d1a74d2c
Teo <thucdo.developer@gmail.com>
2021-12-06 22:37:44 +0700

    base FE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,129 @@
-import './App.css'
+import './App.scss'
 
 function App() {
-    return <div className='App'>hello</div>
+    return (
+        <div className='trello-thucidol'>
+            <nav className='navbar app'>nav</nav>
+            <nav className='navbar board'>board</nav>
+            <div className='board-columns'>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+                <div className='column'>
+                    <header>Brainstorm</header>
+                    <ul>
+                        <li>
+                            <img
+                                src='https://img.freepik.com/free-vector/nature-scene-with-river-hills-forest-mountain-landscape-flat-cartoon-style-illustration_1150-37326.jpg?size=626&ext=jpg'
+                                alt=''
+                            />
+                            Titile: thucidol
+                        </li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                        <li>Add what would you like to work on below</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
