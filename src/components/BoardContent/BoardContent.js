b9d69808f85151193d81ad35d586f963518bcb24
Teo <thucdo.developer@gmail.com>
2021-12-08 20:11:56 +0700

    drag and drop card

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,5 +1,6 @@
 import Column from 'components/Column/Column'
 import React, {useState, useEffect} from 'react'
+import {Container, Draggable} from 'react-smooth-dnd'
 import {isEmpty} from 'lodash'
 import './BoardContent.scss'
 import {initialData} from 'actions/initialData'
@@ -27,11 +28,30 @@ function BoardContent() {
     if (isEmpty(board)) {
         return <div>Board not found</div>
     }
+
+    const onColumnDrop = (dropResult) => {}
+
     return (
         <div className='board-content'>
-            {columns.map((item, index) => {
-                return <Column key={index} column={item} />
-            })}
+            <Container
+                orientation='horizontal'
+                onDrop={onColumnDrop}
+                getChildPayload={(index) => columns[index]}
+                dragHandleSelector='.column-drag-handle'
+                dropPlaceholder={{
+                    animationDuration: 150,
+                    showOnTop: true,
+                    className: 'column-drop-preview',
+                }}
+            >
+                {columns.map((column, index) => {
+                    return (
+                        <Draggable key={index}>
+                            <Column column={column} />
+                        </Draggable>
+                    )
+                })}
+            </Container>
         </div>
     )
 }
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,19 +1,38 @@
 import Cards from 'components/Cards/Cards'
 import React from 'react'
+import {Container, Draggable} from 'react-smooth-dnd'
 import './Column.scss'
 import {mapOrder} from 'utilities/sorts'
 
 function Column(props) {
     const {column} = props
-    const {cards} = mapOrder(column.cards, column.cardOrder, 'id')
+    const cards = mapOrder(column.cards, column.cardOrder, 'id')
+
+    const onCardDrop = (dropResult) => {}
     return (
         <div className='column'>
-            <header>{column.title}</header>
-            <ul className='card-list'>
-                {cards.map((card, index) => (
-                    <Cards key={index} card={card} />
-                ))}
-            </ul>
+            <header className='column-drag-handle'>{column.title}</header>
+            <div className='card-list'>
+                <Container
+                    groupName='thucdo-dev'
+                    onDrop={onCardDrop}
+                    getChildPayload={(index) => cards[index]}
+                    dragClass='card-ghost'
+                    dropClass='card-ghost-drop'
+                    dropPlaceholder={{
+                        animationDuration: 150,
+                        showOnTop: true,
+                        className: 'card-drop-preview',
+                    }}
+                    dropPlaceholderAnimationDuration={200}
+                >
+                    {cards.map((card, index) => (
+                        <Draggable key={index}>
+                            <Cards key={index} card={card} />
+                        </Draggable>
+                    ))}
+                </Container>
+            </div>
             <footer>Add another card</footer>
         </div>
     )
